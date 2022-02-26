d8176ebfa1eebc6e9d992fceae5410437bb623ba
edenfeller <edenfeller@gmail.com>
2022-02-26 13:04:14 +0200

    login page

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { Form, Input, Button, Checkbox } from "antd";
 import { useDispatch } from "react-redux";
 import { login } from "../../features/userSlice";
+import "./Login.css";
+import plates_image from './plates_image.jpg';
+
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -23,40 +26,64 @@ function Login() {
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item label="Username" name="username">
-        <Input value={username} onChange={(e) => setUsername(e.target.value)} />
-      </Form.Item>
+    <div className="login-page">
+      <div className="login-box">
+        <div className="illustration-wrapper">
+          <img
+            src={plates_image}
+            alt="plates_image"
+          />
+        </div>
+        <Form
+          name="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <p className="form-title">Welcome back</p>
+          <p>Login to the Dashboard</p>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Item>
 
-      <Form.Item label="Password" name="password">
-        <Input.Password
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 8, span: 16 }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 }
 
